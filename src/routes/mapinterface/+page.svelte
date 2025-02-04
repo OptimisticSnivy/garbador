46d563350d83f6ebc64cb76c4060113d970d0f77
Anand Mahamuni <m.anand7203@gmail.com>
2025-02-04 20:49:37 +0530

    Implemented Toolbar

--- src/routes/mapinterface/+page.svelte
+++ src/routes/mapinterface/+page.svelte
@@ -2,10 +2,10 @@
 	import img from "$lib/assets/greenTrashMarker.svg";
 	import imgred from "$lib/assets/redTrashMarker.svg";
 	import PocketBase from "pocketbase";
-
-	let map;
 	const pb = new PocketBase("http://127.0.0.1:8090");
 
+	let initialView = [18.5252, 73.8851];
+	let map;
 	let coords = [];
 	async function getCoords() {
 		const records = await pb.collection("sensors").getFullList({});
@@ -61,7 +61,7 @@
 	}
 
 	function createMap(container) {
-		let m = L.map(container).setView([18.5252, 73.8851], 13);
+		let m = L.map(container).setView(initialView, 13);
 		L.tileLayer(
 			"https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png", // Change to something more consistent to the style
 			{
@@ -86,8 +86,54 @@
 	}
 </script>
 
-<div></div>
 <div id="maptile" use:mapAction />
+<div class="toolbar">
+	<button
+		on:click={() => {
+			map.setView(initialView, 13, { animate: true });
+		}}
+		title="Reset-View"
+	>
+		<svg
+			data-slot="icon"
+			aria-hidden="true"
+			fill="none"
+			stroke-width="1.5"
+			stroke="currentColor"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			></path>
+		</svg>
+	</button>
+
+	<!-- Stand-in button, does nothing-->
+	<button title="Refresh">
+		<svg
+			data-slot="icon"
+			aria-hidden="true"
+			fill="none"
+			stroke-width="1.5"
+			stroke="currentColor"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			></path>
+		</svg>
+	</button>
+</div>
 
 <svelte:head>
 	<link
@@ -95,6 +141,11 @@
 		href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
 		integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
 		crossorigin=""
+	/>
+	<link
+		rel="stylesheet"
+		type="text/css"
+		href="https://cdn.kde.org/breeze-icons/icons.css"
 	/>
 	<!-- Make sure you put this AFTER Leaflet's CSS -->
 	<script
@@ -109,5 +160,30 @@
 		height: 782px;
 		width: 100%;
 		margin-top: 30px;
+	}
+
+	.toolbar {
+		position: absolute;
+		top: 100px; /* Adjust position as needed */
+		right: 20px; /* Adjust position as needed */
+		background-color: rgba(255, 255, 255, 0); /* Transparent background */
+		padding: 10px;
+		/* border-radius: 8px; */
+		box-shadow: 0 0px 0px rgba(0, 0, 0, 0.2); /* Slight shadow for better visibility */
+		z-index: 1000; /* Make sure it's on top */
+	}
+
+	button {
+		background-color: #12161a;
+		color: paleturquoise;
+		border: none;
+		margin: 1px;
+		width: 50%;
+		padding: 10px 10px;
+		cursor: pointer;
+	}
+
+	.toolbar button:hover {
+		background-color: #0056b3;
 	}
 </style>
