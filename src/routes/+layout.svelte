e03249a986bf95a60a908581c4cdf53d97074ab7
Anand Mahamuni <m.anand7203@gmail.com>
2025-02-05 19:29:27 +0530

    Fixed "L not defined" on initial launch.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,21 @@
 	import Navbar from "$lib/navbar.svelte";
 </script>
 
+<svelte:head>
+	<link
+		rel="stylesheet"
+		href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+		integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+		crossorigin=""
+	/>
+	<!-- Make sure you put this AFTER Leaflet's CSS -->
+	<script
+		src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+		integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+		crossorigin=""
+	></script>
+</svelte:head>
+
 <Navbar />
 <slot />
 
@@ -11,7 +26,7 @@
 		color: paleturquoise;
 		font-family: "IBM Plex Sans", monospace;
 		display: flex;
-		margin: 0px;							/* adds a fucking margin to the whole page(container) if not specified */
+		margin: 0px; /* adds a fucking margin to the whole page(container) if not specified */
 		flex-direction: column;
 		justify-content: center;
 	}
